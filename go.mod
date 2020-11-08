adfd32f7449784dd2c9d757063e054810f78155d
comboo <291376690@qq.com>
2020-11-09 02:15:18 +0800

    合并代码

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module og
 
 go 1.15
+
+require (
+	github.com/go-pg/pg v8.0.7+incompatible
+	github.com/go-pg/pg/v10 v10.6.2
+)
--- reqeuest/request.go
+++ reqeuest/request.go
@@ -1,9 +1,22 @@
 package req
 
+import "og/item"
+
 type Request struct {
+	tableName struct{} `pg:"job,alias:job"`
+	UUID      string
+	URL       string
+	Host      string
+	Download  string
+	Datas     []*item.Field
+	Status    string // 等待， 调度中， 成功， 失败， 重试
+	Retry     int
+	Log       string
 }
 
 // New 创建一个Request对象, 可以传入任何对象
-func New(i interface{}) *Request {
-	return &Request{}
+func New(URL string) *Request {
+	return &Request{
+		URL: URL,
+	}
 }
